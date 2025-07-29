45d94457b462d6710b04bc304bf5e4972d522fc4
QSChou <qs.chou@innolux.com>
2025-07-29 16:55:04 +0800

    add HelloWorld test case

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+       <h1> {{ msg }} </h1>
+       <button @click="handleClick">Clieck me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld', 
+    props: {
+        msg: String
+    },
+    methods: {
+        handleClick() {
+            this.$emit('clicked')
+        }
+    }
+}
+</script>
